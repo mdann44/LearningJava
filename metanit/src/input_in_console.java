d6947158251db4ef1a96786b98cbe26009e04a7e
mdann44 <120579899+mdann44@users.noreply.github.com>
2022-12-15 08:11:34 +0300

    Дополнил комментарии к файлу input_in_console.java

--- metanit/src/input_in_console.java
+++ metanit/src/input_in_console.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
-public class input_in_console {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите имя: ");
-        String name = in.nextLine();
+import java.util.Scanner; // импортируем Scanner из пакета java.util
+public class input_in_console { // объявили класс
+    public static void main(String[] args) { // объявили метод, внутри которого функции
+        Scanner in = new Scanner(System.in); // создали сканер в функции (для чтения из консоли)
+        System.out.print("Введите имя: "); // отображаем сообщение пользователю
+        String name = in.nextLine(); // считываем из консоли строку
         System.out.print("Введите возраст: ");
-        int age = in.nextInt();
+        int age = in.nextInt(); // считываем из консоли целые числа
         System.out.print("Введите рост, м.: ");
-        float height = in.nextFloat();
-        System.out.printf("Имя: %s Возраст: %d Рост: %f", name, age, height);
+        float height = in.nextFloat(); // считываем из консоли числа с плавающей запятой
+        System.out.printf("Имя: %s Возраст: %d Рост: %f", name, age, height); // выводим суммарную информацию
     }
 }
