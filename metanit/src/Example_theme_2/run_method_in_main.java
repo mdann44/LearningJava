163f57c63ddc908952adfbb3be2298f031d709b9
mdann44 <120579899+mdann44@users.noreply.github.com>
2022-12-16 14:38:25 +0300

    Продолжил конспект в FAQ. Остановился на теме "Методы" Добавил файлы примеров выполнения циклов по одномерным и двухмерным массивам

---
+++ metanit/src/Example_theme_2/run_method_in_main.java
@@ -0,0 +1,16 @@
+package Example_theme_2;
+
+public class run_method_in_main {
+    public static void main (String[] args){
+    hello();
+    welcome();
+    hello();
+    }
+    static void hello(){
+        System.out.println("Hello!");
+        }
+    static void welcome(){
+        System.out.println("Welcome!");
+    }
+    }
+
