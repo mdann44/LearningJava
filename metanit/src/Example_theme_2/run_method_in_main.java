9bb99f269767d93bb0d0fcc7d68aa1ad1d40ee68
mdann44 <120579899+mdann44@users.noreply.github.com>
2022-12-18 12:35:04 +0300

    Восстановил, нечаянно, удаленные файлы. Продолжил конспект в FAQ. Остановился на теме "Введение в обработку исключений" Добавил файлы примеров работы с оператором return, параметрами методов и запуску различных методов в main

--- metanit/src/Example_theme_2/run_method_in_main.java
+++ metanit/src/Example_theme_2/run_method_in_main.java
@@ -1,15 +1,15 @@
 package Example_theme_2;
 
 public class run_method_in_main {
-    public static void main (String[] args){
+    public static void main (String[] args){ // Основной метод, вызывающий остальные
         hello();
         welcome();
         hello();
     }
-    static void hello(){
+    static void hello(){ // Вызываемый метод
         System.out.println("Hello!");
     }
-    static void welcome(){
+    static void welcome(){ // Вызываемый метод
         System.out.println("Welcome!");
     }
 }
