072036a4e87238f9e3ea90da0724f0e5a125876c
mdann44 <120579899+mdann44@users.noreply.github.com>
2022-12-22 11:16:05 +0300

    StartClassForPerson.java - добавил комментарии

--- metanit/src/Q3ClassObjectConstructor/StartClassForPerson.java
+++ metanit/src/Q3ClassObjectConstructor/StartClassForPerson.java
@@ -4,12 +4,12 @@ package Q3ClassObjectConstructor;
 
 public class StartClassForPerson {
     public static void main(String[] args) {
-        ObjectPerson vasya = new ObjectPerson("Вася;", 18);
+        ObjectPerson vasya = new ObjectPerson("Вася;", 18);//Создал несколько объектов через конструктор
         ObjectPerson petya = new ObjectPerson("Петя;", 28);
         ObjectPerson kolya = new ObjectPerson("Коля;", 43);
         ObjectPerson agent007 = new ObjectPerson("Женька Болт;",1007);
 
-        System.out.println("Имя: "+vasya.name+" Возраст:  "+vasya.age);
+        System.out.println("Имя: "+vasya.name+" Возраст:  "+vasya.age);//Вывел информацию о созданных объектах
         System.out.println("Имя: "+petya.name+" Возраст:  "+petya.age);
         System.out.println("Имя: "+kolya.name+" Возраст:  "+kolya.age);
         System.out.println("Имя: "+agent007.name+" Возраст:  "+agent007.age);
